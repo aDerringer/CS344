68dfd4172c2ce51e0ff2d241df7e99c097d9cf17
Andrew Derringer <andrew.derringer@global-formula-racing.com>
2019-11-20 01:38:26 -0800

    ready to submit smallsh

--- qt3/helpers.h
+++ qt3/helpers.h
@@ -6,5 +6,9 @@ int string_to_arr(char*, char**);
 void free_string_arr(char**, int);
 void print_arr(char**, int);
 void nav_chcwd(char**, int);
-
+void expand_pid(char*, char*, int); 
+int fork_router(char**, int*, int*, int*, char*);
+int exit_status(int, int);
+void fork_exec(char**, int, char*);
+ 
 #endif
